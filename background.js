310a836788fbecaa2799e8935ccdbea2c6e8965b
William Guss <carlostnb@gmail.com>
2014-12-04 00:49:33 -0700

    naturalistic voting mechanisms complete; ready for distro.

--- background.js
+++ background.js
@@ -3,17 +3,23 @@
 
 
 //clears the cookies on gorilla :)
-function clearGorillaCookies(){
+function clearGorillaCookies(tab){
 	console.log("taking cookies out of the cookie jar");
 
-	chrome.cookies.getAll({'url': '\thttp://www.gorillamusic.com'}, function(cookies){
+	chrome.cookies.getAll({'url': 'http://www.gorillamusic.com'}, function(cookies){
 		cookies.forEach(function(chocolatechip){
 			console.log('removing '+ chocolatechip.name);
 			chrome.cookies.remove({
 				url: 'http://www.gorillamusic.com',
 			 	name:chocolatechip.name
 			});
-		})
+
+		});
+
+
+		//Now let's register an account!
+		registerAccount(tab);
+
 	});
 }
 
@@ -22,12 +28,39 @@ function registerAccount(tab){
 	//change the page bro
 	chrome.tabs.update(tab.id, {url: 'http://www.gorillamusic.com/register/'},
 		function(registerTab){
+			console.log("registering");
 			//Let's fill in the page now with some cool stuff!
-			chrome.tabs.executeScript(registerTab.id, {file: ''})
+			chrome.tabs.executeScript(registerTab.id, {file: 'register.js'}, function(res){
+				//Finally let's vote for the pelicant's
+				setTimeout(function(){
+				    //after 10 seconds call the vote function
+				    vote(tab);
+				}, 25000);
+			});
 	});	
 }
 
+function vote(tab){
+	//change the page bro
+	chrome.tabs.update(tab.id, {url: 'http://www.gorillamusic.com/?contestants=the-pelicants'},
+		function(registerTab){
+			console.log("voting");
+			//Let's fill in the page now with some cool stuff!
+			chrome.tabs.executeScript(registerTab.id,
+			 {code: 'document.getElementById("vote10057").click();'},
+			 function(res){
+				console.log("finished");
+				count++;
+				if(count < 100)
+				setTimeout(function(){
+						clearGorillaCookies(tab);
+				}, 5000);
 
+			 });
+	});	
+}
+
+var count = 0;
 function click(e){
 	console.log("background.js : click()");
 	//get the current tab.
@@ -37,12 +70,7 @@ function click(e){
 		var re = /.*gorilla/;
 		if(re.test(tab.url)){
 			//Time to clear those cookies!
-			//clearGorillaCookies();
-
-			//Now let's register an account!
-			registerAccount(tab);
-
-			//Finally let's vote for the pelicant's
+			clearGorillaCookies(tab);
 		}
 		else
 			chrome.tabs.executeScript({
